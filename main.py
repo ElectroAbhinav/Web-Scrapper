900310e637a7c8cb8585aee6a70e9e3abc9adf52
Abhinav <itsofficialabhinav@gmail.com>
2021-03-09 09:51:03 +0530

    Filtering out unfamiliar skills

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+print("Put some skills that you're not familiar with :->")
+unfamiliar_skill = input('>')
+print(f"Filtering Out: {unfamiliar_skill}")
+
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=&cboWorkExp1=0').text
 
 soup = BeautifulSoup(html_text, 'lxml')
@@ -8,15 +12,11 @@ jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 for job in jobs:
     time_posted = job.find('span', class_='sim-posted').span.text
     if 'few' in time_posted:
-
-        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
-        skills = job.find('span', class_='srp-skills').text.replace(' ','')
-
-
-        print(f'''
-        Company Name: {company_name}
-        Required Skills: {skills}
-        Time Posted: {time_posted}
-        ''')
-
-        print(' ')
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+        skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+        more_info = job.header.h2.a['href']
+        if unfamiliar_skill not in skills:
+            print(f"Company Name: {company_name.strip()}")
+            print(f"Required Skills: {skills.strip()}")
+            print(f"More Information: {more_info}")
+            print(' ')
