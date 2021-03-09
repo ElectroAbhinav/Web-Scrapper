ea7b076d97c3272d9cc949a56382a71d5e1fee2d
Abhinav <itsofficialabhinav@gmail.com>
2021-03-09 09:31:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from bs4 import BeautifulSoup
+import requests
 
-# Reading a File from HTML
-with open('home.html', 'r') as html_file:
-    content = html_file.read()
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=&cboWorkExp1=0').text
 
-    soup = BeautifulSoup(content, 'lxml')
-    course_cards = soup.find_all('div', class_='card')
-    for course in course_cards:
-        print(course.h5)
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+for job in jobs:
+    time_posted = job.find('span', class_='sim-posted').span.text
+    if 'few' in time_posted:
 
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
+        skills = job.find('span', class_='srp-skills').text.replace(' ','')
+
+
+        print(f'''
+        Company Name: {company_name}
+        Required Skills: {skills}
+        Time Posted: {time_posted}
+        ''')
+
+        print(' ')
